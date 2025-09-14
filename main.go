86718031602e2bd63f0fd9a8d1d8268cf06c0eb5
Jayushi-Jain <jayushinimboni21@gmail.com>
2025-09-14 16:01:13 +0530

    remove unwanted files

--- main.go
+++ main.go
@@ -1,6 +1,12 @@
 package main
 
 func main() {
-	nums := []int{1, 2, 4, 6}
+	nums := []int{-1, 0, 1, 2, -1, -4}
+
+	temp := make(map[int][]int)
+
+	for _, num := range nums {
+		temp[num] = append(temp[num], num)
+	}
 
 }
